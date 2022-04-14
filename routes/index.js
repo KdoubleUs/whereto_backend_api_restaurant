d9f772fb9b0961c23a778beb6619e59ea6faf9b8
KW <88893976+KdoubleUs@users.noreply.github.com>
2022-04-14 14:46:23 -0400

    fix bugs / routing issues

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { append } from "express/lib/response";
-import activitiesRoute from "./activities";
-import barsRoute from "./bars";
-import restaurantRoute from "./restaurants";
+import activitiesRoute from "./activities.js";
+import barsRoute from "./bars.js";
+import restaurantRoute from "./restaurants.js";
 const router = Router();
 
-router.get("/", res.send(`this is the api root`));
+router.get("/", (req, res) => res.send(`this is the api root`));
 router.use("/", activitiesRoute);
 router.use("/", barsRoute);
 router.use("/", restaurantRoute);
